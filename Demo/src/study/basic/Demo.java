c76e0f43bf553c9678b7a5fb26212f6608004d45
vlong1808 <volong1808@gmail.com>
2026-02-03 01:40:29 +0700

    Update README.md

--- Demo/src/study/basic/Demo.java
+++ Demo/src/study/basic/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args){
 		
 		int a = 10;
-		System.out.println("Hello World! " + a + " Barbara");
+		System.out.println("Hello World! ");
 	}
 
 }
